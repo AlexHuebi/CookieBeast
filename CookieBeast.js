6bc0da354005dd9b92b56ac142107138745d0252
AlexHuebi <39099705+AlexHuebi@users.noreply.github.com>
2019-12-09 19:28:44 +0100

    Update CookieBeast.js
    now resets the clicked cookies
    Why? I dunno...

--- CookieBeast.js
+++ CookieBeast.js
@@ -1,1 +1,1 @@
-if(typeof CookieWizard === 'undefined'){Game.LoadMod('https://ghxx.github.io/CookieWizard/CookieWizard.js');}for(var i = 0; i<5; i++){var autoclick= setInterval(function(){Game.ClickCookie();}, 1);}
+if(typeof CookieWizard === 'undefined'){Game.LoadMod('https://ghxx.github.io/CookieWizard/CookieWizard.js');}for(var i = 0; i<5; i++){Game.cookieClicks = 0; var autoclick= setInterval(function(){Game.ClickCookie();}, 1);}
